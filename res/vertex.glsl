97ab36660cc95423605a4d30276d0c5e42dd4081
Nathan Bellaiche <nathanbellaiche000@gmail.com>
2024-05-24 18:26:33 +0200

    Caméra et Matrices

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -1,18 +1,9 @@
 #version 410
 
 layout(location = 0) in vec2 in_position;
-uniform float aspect_ratio;
-uniform float time;
+uniform mat4 view_projection_matrix;
 
 void main()
-{
-    float radius = 1.0;
-    float speed = 1.0;
-    
-    vec2 position = in_position;
-
-    position.x = position.x / aspect_ratio + radius * cos(time * speed) / aspect_ratio;
-    position.y += radius * sin(time * speed);
-
-    gl_Position = vec4(position, 0., 1.);
+{ 
+    gl_Position = view_projection_matrix * vec4(in_position, 0., 1.);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "opengl-framework/opengl-framework.hpp" // Inclue la librairie qui va nous servir à faire du rendu
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
 
 int main()
 {
@@ -6,33 +8,12 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
-    glEnable(GL_BLEND);
-    glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE_MINUS_DST_ALPHA, GL_ONE);
-
-    auto const background_shader = gl::Shader{{
-        .vertex   = gl::ShaderSource::File{"res/background_vertex.glsl"},
-        .fragment = gl::ShaderSource::File{"res/background_fragment.glsl"},
-    }};
+    auto camera = gl::Camera{};
+    gl::set_events_callbacks({camera.events_callbacks()});
 
     auto const shader = gl::Shader{{
         .vertex   = gl::ShaderSource::File{"res/vertex.glsl"},
         .fragment = gl::ShaderSource::File{"res/fragment.glsl"},
-    }};
-
-    auto const fullscreen_quad = gl::Mesh{{
-        .vertex_buffers = {{
-            .layout = {gl::VertexAttribute::Position2D{0}},
-            .data   = {
-                -1.0f, -1.0f,
-                +1.0f, -1.0f,
-                +1.0f, +1.0f,
-                -1.0f, +1.0f
-            },
-        }},
-        .index_buffer = {
-            0, 1, 2,
-            0, 2, 3
-        },
     }};
 
     auto const rectangle_mesh  = gl::Mesh{{
@@ -53,12 +34,31 @@ int main()
 
     while (gl::window_is_open())
     {
-        background_shader.bind();
-        fullscreen_quad.draw();
+        glClearColor(0.f, 0.f, 1.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glm::mat4 const view_matrix = camera.view_matrix();
+        //glm::mat4 const projection_matrix = glm::infinitePerspective(1.f /*field of view in radians*/, gl::framebuffer_aspect_ratio() /*aspect ratio*/, 0.001f /*near plane*/);
+        glm::mat4 const projection_matrix = glm::ortho(
+            -10.0f, 10.0f,
+            -10.0f, 10.0f,
+            0.1f, 1000.0f
+        );
+
+        glm::mat4 const rotation = glm::rotate(glm::mat4{1.f}, gl::time_in_seconds(), glm::vec3{0.f, 0.f, 1.f});
+        glm::mat4 const translation = glm::translate(glm::mat4{1.f}, glm::vec3{0.f, 3.f, 0.f});
+
+        // Combinaison des matrices - Rotation suivie de Translation
+        glm::mat4 model_matrix_RT = translation * rotation;
+
+        // Combinaison des matrices - Translation suivie de Rotation
+        glm::mat4 model_matrix_TR = rotation * translation;
+
+        glm::mat4 model_view_projection_matrix = projection_matrix * view_matrix * model_matrix_TR;
 
         shader.bind(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
-        shader.set_uniform("aspect_ratio", gl::framebuffer_aspect_ratio());
-        shader.set_uniform("time", gl::time_in_seconds());
+        shader.set_uniform("view_projection_matrix", model_view_projection_matrix);
+
         rectangle_mesh.draw(); // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
     }
 }
