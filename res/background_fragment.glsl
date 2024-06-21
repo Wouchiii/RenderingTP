02d40e78408fc28158ea7a7f2ef80f3e49823106
Nathan Bellaiche <nathanbellaiche000@gmail.com>
2024-06-21 09:03:28 +0200

    Post-Process (Rouge)

--- res/background_fragment.glsl
+++ res/background_fragment.glsl
@@ -6,6 +6,5 @@ uniform sampler2D screen_texture;
 
 void main() {
     vec4 texColor = texture(screen_texture, uv);
-    float grayscale = dot(texColor.rgb, vec3(0.299, 0.587, 0.114));
-    out_color = vec4(grayscale, grayscale, grayscale, 1.0);
+    out_color = vec4(texColor.r, 0.0, 0.0, 1.0);
 }
