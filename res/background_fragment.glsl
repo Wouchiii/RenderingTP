408368562819407629fe0cc2e27491bfcb7c9eed
Nathan Bellaiche <nathanbellaiche000@gmail.com>
2024-06-21 09:02:20 +0200

    Post-Process (Noir et Blanc)

--- res/background_fragment.glsl
+++ res/background_fragment.glsl
@@ -5,5 +5,7 @@ out vec4 out_color;
 uniform sampler2D screen_texture;
 
 void main() {
-    out_color = texture(screen_texture, uv);
+    vec4 texColor = texture(screen_texture, uv);
+    float grayscale = dot(texColor.rgb, vec3(0.299, 0.587, 0.114));
+    out_color = vec4(grayscale, grayscale, grayscale, 1.0);
 }
